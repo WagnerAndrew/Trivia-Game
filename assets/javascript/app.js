1b7fd514b82342fec1bbef03e1f1f7de0b678bb0
WagnerAndy <wagner.andrew.james@gmail.com>
2018-11-17 20:58:27 -0700

    Everything but countdown timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,30 @@
 // VARIABLES
 
+
+
 var correct = 0;
 var incorrect = 0;
 
-var roundCount = 2;
-
-        var questionArr = [
-            {
-                question: "What is the composer Vivaldi's first name?",
-                answerArr: ["Maricio","Pietro","Antonio","Laurice"],
-                correctAnswer: "Antonio"
-            },
-            {
-                question: "Who made the highly rated 1959 jazz album 'Kind Of Blue?'",
-                answerArr: ["John Coltrane","Miles Davis","Ornette Coleman","Duke Ellington"],
-                correctAnswer: "Miles Davis"
-            },
-            {
-                question: "In which American city was Elvis discovered in?",
-                answerArr: ["Nashville","Macom","Charleston","Memphis"],
-                correctAnswer: "Memphis"
-            },
+    var questionArr = [
+        {
+            question: "What is the composer Vivaldi's first name?",
+            answerArr: ["Maricio","Pietro","Antonio","Laurice"],
+            correctAnswer: "Antonio"
+        },
+        {
+            question: "Who made the highly rated 1959 jazz album 'Kind Of Blue?'",
+            answerArr: ["John Coltrane","Miles Davis","Ornette Coleman","Duke Ellington"],
+            correctAnswer: "Miles Davis"
+        },
+        {
+            question: "In which American city was Elvis discovered in?",
+            answerArr: ["Nashville","Macom","Charleston","Memphis"],
+            correctAnswer: "Memphis"
+        },
         ];
+
+var roundCount = (questionArr.length - 1);
+console.log ("Round count is: " + roundCount);        
 
 // FUNCTIONS
 
@@ -30,21 +33,27 @@ var roundCount = 2;
 
 function Question () {
 
-    $("#displayText").empty ();
-    displayText = $("<div>");
+    if (roundCount < 0){
+        Reset ();
+    }else{
 
-    displayText.append("<h2>" + questionArr[roundCount].question + "</h2>");
-    var answerText = $("<h3>");
 
-    for (i = 0 ; i < questionArr[roundCount].answerArr.length; i++) {
+                $("#displayText").empty ();
+                displayText = $("<div>");
 
-        var answerButton = $("<button type='button' class='btn-light m-2'>" + questionArr[roundCount].answerArr[i] +"</button><"); 
-        answerButton.attr("data-value", questionArr[roundCount].answerArr[i]);
-        answerText.append(answerButton);  
+                displayText.append("<h2>" + questionArr[roundCount].question + "</h2>");
+                var answerText = $("<h3>");
+
+                for (i = 0 ; i < questionArr[roundCount].answerArr.length; i++) {
+
+                    var answerButton = $("<button type='button' class='btn-light m-2'>" + questionArr[roundCount].answerArr[i] +"</button><"); 
+                    answerButton.attr("data-value", questionArr[roundCount].answerArr[i]);
+                    answerText.append(answerButton);  
+                };
+
+                $("#displayText").append(displayText);
+                $("#displayText").append(answerText);
     };
-
-    $("#displayText").append(displayText);
-    $("#displayText").append(answerText);
 };
 
 // FUNCTION 2
@@ -58,6 +67,11 @@ function Correct () {
     displayText.append("<h4>" + questionArr[roundCount].question + "</h4>");
     displayText.append("<h2><strong>" + questionArr[roundCount].correctAnswer + "</strong></h2>");
     $("#displayText").append(displayText);
+
+    correct++;
+    roundCount--;
+    console.log ("Round count is: " + roundCount);
+    setTimeout(Question, 1000 * 3);
 };
 
 // FUNCTION 3
@@ -71,6 +85,31 @@ function Incorrect () {
     displayText.append("<h4>" + questionArr[roundCount].question + "</h4>");
     displayText.append("<h2><strong>" + questionArr[roundCount].correctAnswer + "</strong></h2>");
     $("#displayText").append(displayText);
+
+    incorrect++;
+    roundCount--;
+    console.log ("Round count is: " + roundCount);
+    setTimeout(Question, 1000 * 3);
+};
+
+// FUNCTION 3
+
+function Reset () {
+
+    $("#displayText").empty ();
+    displayText = $("<div>");
+
+    displayText.append("<h2>" + "Game Over" + "</h2>");
+    displayText.append("<h3>" + "You got " + correct + " right & you missed " + incorrect + " !" + "</h3>");
+
+    $("#displayText").append(displayText);
+
+    
+    roundCount = (questionArr.length -1);
+    correct = 0;
+    incorrect = 0;
+
+    setTimeout(Question, 1000 * 3);
 };
 
 
@@ -80,26 +119,15 @@ function Incorrect () {
 
 $("#startButton").on("click", Question);
 
-$("#displayText").on("click", ".btn-light", function(event) {
+$("#displayText").on("click", ".btn-light", function (event) {
     event.preventDefault();
     var buttonValue = $(this).attr("data-value");
-    
-    console.log("Button value is: " + buttonValue);
 
-    if ($(this).attr("data-value") === questionArr[roundCount].correctAnswer){
-        Correct ();
-        correct++;
-        roundCount--;
-    
-        console.log ("The correct count is: "+correct+"The round count is: "+roundCount);
-
-        setTimeout(Question, 1000 * 3);
-        
-    }else{
-        Incorrect ();
-        incorrect++;
-        roundCount--;
-        setTimeout(Question, 1000 * 3);
-    };
+        if ($(this).attr("data-value") === questionArr[roundCount].correctAnswer) {
+            Correct();
+        } else {
+            Incorrect();
+        };
 
 });
+
